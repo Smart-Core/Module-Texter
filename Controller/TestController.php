885d311dd9f257175e442d68814b2f3eca91e78f
Smart-Core <artem.digi@gmail.com>
2012-06-15 04:54:41 +0700

    test hello action.

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -8,8 +8,15 @@ use SmartCore\Bundle\EngineBundle\Controller\Controller;
 class TestController extends Controller // Module
 {
     public function indexAction($text = 888)
+    {
+        return $this->render("SmartCoreTexterModule::texter.html.twig", array(
+            'text' => $text,
+        ));
+    }
+
+    public function helloAction($text = 'Hello')
 	{
-		return $this->render("SmartCoreTexterModule::texter.html.twig", array(
+		return $this->render("SmartCoreTexterModule::hello.html.twig", array(
             'text' => $text,
         ));
 	}
