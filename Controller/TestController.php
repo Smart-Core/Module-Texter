e222d2e4e1d5ee410ec17a0079f985f0b1c67b97
Smart-Core <artem.digi@gmail.com>
2012-06-29 19:27:19 +0700

    текучка...

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -3,10 +3,33 @@
 namespace SmartCore\Module\Texter\Controller;
 
 use SmartCore\Bundle\EngineBundle\Controller\Module;
-//use SmartCore\Bundle\EngineBundle\Controller\Controller;
+use SmartCore\Bundle\EngineBundle\Response;
 
-class TestController extends Module // Controller
+class TestController extends Module
 {
+    /**
+     * Конструктор.
+     * 
+     * @return void
+     */
+    protected function init()
+    {
+        //$this->setVersion(0.4);
+        
+        $this->NodeProperties->setDefaultParams(array(
+            'text_item_id'    => 0,
+            'editor'        => 1,
+        ));
+
+        /*
+        $this->View->setOptions(array(
+            'engine'        => 'simple',
+            'template_ext' => '.tpl',
+        ));
+        */
+        $this->View->setRenderMethod('echoProperties');
+    }
+
     public function indexAction($text = 888)
     {
         return $this->render("SmartCoreTexterModule::texter.html.twig", array(
@@ -15,9 +38,16 @@ class TestController extends Module // Controller
     }
 
     public function helloAction($text = 'Hello')
-	{
-		return $this->render("SmartCoreTexterModule::hello.html.twig", array(
+    {
+        return $this->render("SmartCoreTexterModule::hello.html.twig", array(
             'text' => $text,
         ));
-	}
+    }
+
+    public function testAction($text = 'Hello')
+    {
+        $this->View->text = $text;
+
+        return new Response($this->View);
+    }
 }
